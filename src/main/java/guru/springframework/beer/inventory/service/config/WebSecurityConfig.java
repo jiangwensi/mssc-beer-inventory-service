f6d9f147fefbb7f19e1ed357fcad98089fa89d8b
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-20 15:24:29 +0800

    secure get inventory endpoint with spring security

---
+++ src/main/java/guru/springframework/beer/inventory/service/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Jiang Wensi on 20/11/2020
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
